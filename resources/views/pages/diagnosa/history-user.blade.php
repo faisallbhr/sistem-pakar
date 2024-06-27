970973918ca815c3f03434087c8c2deff8af1a32
Faisal Bahari <fbhr1112@gmail.com>
2024-06-27 16:37:58 +0700

    style: nowrap text pdf

--- resources/views/pages/diagnosa/history-user.blade.php
+++ resources/views/pages/diagnosa/history-user.blade.php
@@ -28,7 +28,179 @@
                 </a>
             @endrole
         </div>
+
+        <div id="chart" class="mt-10 -ml-2 mr-12"></div>
     </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    {{-- <script>
+        var options = {
+            series: [{
+                    name: "Gangguan Mood",
+                    data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+                },
+                {
+                    name: "Depresi Ringan",
+                    data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+                },
+                {
+                    name: 'Depresi Sedang',
+                    data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+                }
+            ],
+            chart: {
+                height: 350,
+                type: 'line',
+                zoom: {
+                    enabled: false
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                width: [5, 5, 5],
+                curve: 'straight',
+            },
+            title: {
+                text: 'Statistik Depresi',
+                align: 'left'
+            },
+            legend: {
+                tooltipHoverFormatter: function(val, opts) {
+                    return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+                        '</strong>'
+                }
+            },
+            markers: {
+                size: 0,
+                hover: {
+                    sizeOffset: 6
+                }
+            },
+            xaxis: {
+                categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+                    '10 Jan', '11 Jan', '12 Jan'
+                ],
+            },
+            tooltip: {
+                y: [{
+                        title: {
+                            formatter: function(val) {
+                                return val
+                            }
+                        }
+                    },
+                    {
+                        title: {
+                            formatter: function(val) {
+                                return val
+                            }
+                        }
+                    },
+                    {
+                        title: {
+                            formatter: function(val) {
+                                return val;
+                            }
+                        }
+                    }
+                ]
+            },
+            grid: {
+                borderColor: '#f1f1f1',
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+    </script> --}}
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Data passed from the controller
+            var chartData = @json($chartData);
+
+            var options = {
+                series: [{
+                        name: "Gangguan Mood",
+                        data: chartData.series['Gangguan Mood'] || []
+                    },
+                    {
+                        name: "Depresi Ringan",
+                        data: chartData.series['Depresi Ringan'] || []
+                    },
+                    {
+                        name: 'Depresi Sedang',
+                        data: chartData.series['Depresi Sedang'] || []
+                    }
+                ],
+                chart: {
+                    height: 350,
+                    type: 'line',
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    width: [5, 5, 5],
+                    curve: 'straight'
+                },
+                title: {
+                    text: 'Statistik Depresi',
+                    align: 'left'
+                },
+                legend: {
+                    tooltipHoverFormatter: function(val, opts) {
+                        return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts
+                            .dataPointIndex
+                        ] + '</strong>'
+                    }
+                },
+                markers: {
+                    size: 0,
+                    hover: {
+                        sizeOffset: 6
+                    }
+                },
+                xaxis: {
+                    categories: chartData.dates || [],
+                },
+                tooltip: {
+                    y: [{
+                            title: {
+                                formatter: function(val) {
+                                    return val;
+                                }
+                            }
+                        },
+                        {
+                            title: {
+                                formatter: function(val) {
+                                    return val;
+                                }
+                            }
+                        },
+                        {
+                            title: {
+                                formatter: function(val) {
+                                    return val;
+                                }
+                            }
+                        }
+                    ]
+                },
+                grid: {
+                    borderColor: '#f1f1f1',
+                }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+            chart.render();
+        });
+    </script>
 
     <script>
         $(document).ready(function() {
